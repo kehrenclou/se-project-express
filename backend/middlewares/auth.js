ee76af38c22c37f9accd4fc58973c8e22bed51a8
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-04-01 13:21:30 -0700

    refactor: add centralized error handling to auth.js middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,7 @@
 //backend/middlewares/auth.js
 /* --------------------------------- imports -------------------------------- */
 const jwt = require("jsonwebtoken");
+const UnauthorizedError = require("../errors/unauthorized");
 const { UNAUTHORIZED } = require("../utils/statuses");
 const JWT_SECRET = require("../utils/config");
 
@@ -8,23 +9,23 @@ const JWT_SECRET = require("../utils/config");
 
 //verify token from headers
 //if token ok, middleware should add token payload to the user object and call next()
-const handleAuthError = (res) => {
-  res.status(UNAUTHORIZED).send({ message: "Authorization Required" });
-};
+// const handleAuthError = (res) => {
+//   res.status(UNAUTHORIZED).send({ message: "Authorization Required" });
+// };
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return handleAuthError(res);
+    throw new UnauthorizedError("Unauthorized");
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
-  console.log("modexport called bend auth");
+
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return handleAuthError(res);
+    throw new UnauthorizedError("Unauthorized");
   }
 
   req.user = payload; //asign payload to request object
