ec418b342c7fc51566a1855e9384b091fc0f08d7
Krista Ehrenclou <krista.ehrenclou9@outlook.com>
2023-04-03 16:33:00 -0700

    lint

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,26 @@
 // backend/app.js
 /* --------------------------------- imports -------------------------------- */
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-const helmet = require("helmet");
-const cors = require("cors");
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const cors = require('cors');
 // const rateLimit = require("express-rate-limit");
-const { limiter } = require("./utils/rate-limit-config");
+const { errors } = require('celebrate');
+const { limiter } = require('./utils/rate-limit-config');
 // const path = require('path');
-const { errors } = require("celebrate");
-const { requestLogger, errorLogger } = require("./middlewares/logger");
-const { createUser, loginUser } = require("./controllers/users");
-const usersRouter = require("./routes/users");
-const cardsRouter = require("./routes/cards");
-const auth = require("./middlewares/auth");
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { createUser, loginUser } = require('./controllers/users');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const auth = require('./middlewares/auth');
 const {
   validateUserBody,
   validateLoginBody,
-} = require("./middlewares/validation");
-const { NotFoundError } = require("./errors/not-found");
-const errorHandler = require("./middlewares/error-handler");
+} = require('./middlewares/validation');
+const { NotFoundError } = require('./errors/not-found');
+const errorHandler = require('./middlewares/error-handler');
 
 /* -------------------------- declare app and port -------------------------- */
 /* ------------------------------ connect to DB ----------------------------- */
@@ -29,7 +29,7 @@ const app = express();
 const { PORT = 3000 } = process.env;
 
 // mongoose.connect("mongodb://localhost:27017/aroundb");//older node versions
-mongoose.connect("mongodb://127.0.0.1/aroundb");
+mongoose.connect('mongodb://127.0.0.1/aroundb');
 
 /* -------------------------------- app -------------------------------- */
 // Apply the rate limiting middleware to all requests
@@ -37,7 +37,7 @@ app.use(limiter);
 
 app.use(helmet());
 app.use(cors());
-app.options("*", cors()); // enable requests for all routes
+app.options('*', cors()); // enable requests for all routes
 // app.use(express.static(path.join(__dirname, 'public')));//use for static files
 
 app.use(express.json()); // for versions express 4.16+ can use this instead of bodyparser
@@ -46,22 +46,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(requestLogger);
 
 // crash test - remove after review paases
-app.get("/crash-test", () => {
+app.get('/crash-test', () => {
   setTimeout(() => {
-    throw new Error("Server will crash now");
+    throw new Error('Server will crash now');
   }, 0);
 });
 
 // routes
-app.post("/signup", validateUserBody, createUser);
-app.post("/signin", validateLoginBody, loginUser);
+app.post('/signup', validateUserBody, createUser);
+app.post('/signin', validateLoginBody, loginUser);
 
-app.use("/users", auth, usersRouter);
-app.use("/cards", auth, cardsRouter);
+app.use('/users', auth, usersRouter);
+app.use('/cards', auth, cardsRouter);
 
 // check if this is ok for 404 route
 app.use((req, res, next) => {
-  next(new NotFoundError("This route does not exist"));
+  next(new NotFoundError('This route does not exist'));
 });
 
 app.use(errorLogger); // winston
--- backend/utils/rate-limit-config.js
+++ backend/utils/rate-limit-config.js
@@ -1,5 +1,5 @@
-const rateLimit = require("express-rate-limit");
-//used for express-rate-limit
+const rateLimit = require('express-rate-limit');
+// used for express-rate-limit
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
